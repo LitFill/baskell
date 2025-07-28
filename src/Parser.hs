0ee2b3248d13acdda8d7cc474636bf59a4657050
LitFill <marrazzy54@gmail.com>
2025-07-28 21:35:40 +0700

    feat: add class decl

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,4 +1,5 @@
 module Parser where
+
 import Data.Text (Text)
 
 -- | the ast of neo haskell
@@ -7,11 +8,11 @@ data Module = Module
         -- ^ from @module 'Name'@
     , moduleexports :: [Text]
         -- ^ from @export : '...'@ space separated names
-    , imports :: [(Text, Maybe Text, [Text])]
+    , imports       :: [(Text, Maybe Text, [Text])]
         -- ^ from @import : \n'...'@ newline separated, args: @module name@, optional @rename@, list of imported @decls@
-    , openimports :: [(Text, [Text])]
+    , openimports   :: [(Text, [Text])]
         -- ^ from @open : \n'...'@ newline separated, args: @module name@, list of imported @decls@
-    , moduledecls :: [Declaration]
+    , moduledecls   :: [Declaration]
         -- ^ list of this module declarations
     } deriving (Eq, Show)
 
@@ -22,6 +23,8 @@ data Declaration
         -- ^ args: type alias @name@, list of @type var@, real @type@ name, list of @type arg@
     | DataDecl DataDeclaration
         -- ^ delegates to @DataDeclaration@
+    | ClassDecl [Text] Text [Text] [ClassMember]
+        -- ^ args: list of @constraint@, the class @name@, list of class @arg@, list of class @member@
     deriving (Eq, Show)
 
 data DataConstr
@@ -35,3 +38,10 @@ data DataDeclaration
     | ProductData Text [Text] Text [(Text, Text)] [Text]
         -- ^ args: type @name@, list of @type var@, @constructor@, list of @(field, type)@, list of @deriving@
     deriving (Eq, Show)
+
+data ClassMember = ClassMember
+    { cmname :: Text
+    , cmtype :: Text
+    , cmdefaulttype :: Maybe Text
+    , cmbinding :: Maybe Binding
+    } deriving (Eq, Show)
